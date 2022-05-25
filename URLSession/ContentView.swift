7bacc097be054d9110d3a76536fcece23ed4d40c
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-05-25 18:59:23 +0300

    Initial Commit

---
+++ URLSession/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URLSession
+//
+//  Created by Bayram Yilmaz on 25/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ URLSession/URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionApp.swift
+//  URLSession
+//
+//  Created by Bayram Yilmaz on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
